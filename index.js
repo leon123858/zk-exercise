3b78f960caf9b5ba4c0c772749ad08ce3ee2a336
leon123858 <a0970785699@gmail.com>
2023-03-04 03:01:43 +0800

    update

---
+++ index.js
@@ -0,0 +1,27 @@
+const snarkjs = require('snarkjs');
+const fs = require('fs');
+
+async function run() {
+	const { proof, publicSignals } = await snarkjs.groth16.fullProve(
+		{ a: 1, b: 1 },
+		'circuit.wasm',
+		'circuit_final.zkey'
+	);
+
+	console.log('Proof: ');
+	console.log(JSON.stringify(proof, null, 1));
+
+	const vKey = JSON.parse(fs.readFileSync('verification_key.json'));
+
+	const res = await snarkjs.groth16.verify(vKey, publicSignals, proof);
+
+	if (res === true) {
+		console.log('Verification OK');
+	} else {
+		console.log('Invalid proof');
+	}
+}
+
+run().then(() => {
+	process.exit(0);
+});
