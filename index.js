a0f2ab505d0faa48f7385e5c9020a4d4160b9065
leon123858 <a0970785699@gmail.com>
2023-03-06 12:33:28 +0800

    clear sample

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 const snarkjs = require('snarkjs');
 const fs = require('fs');
+const { assert } = require('console');
 
 async function run() {
+	/**
+	 * Provider: 利用 secret 產生結果與證明
+	 */
+	const secrets = [15, 22];
+
 	const { proof, publicSignals } = await snarkjs.groth16.fullProve(
-		{ a: 1, b: 1 },
+		{ a: secrets[0], b: secrets[1] },
 		'circuit.wasm',
 		'circuit_final.zkey'
 	);
 
-	console.log('Proof: ');
-	console.log(JSON.stringify(proof, null, 1));
+	assert(publicSignals[0] == secrets[0] * secrets[1], 'signal should right');
+	// console.log('Proof: ');
+	// console.log(JSON.stringify(proof, null, 1));
 
+	/**
+	 * 驗證者: 利用 proof 驗證 publicSignals 有效, 無需提交 secrets
+	 */
 	const vKey = JSON.parse(fs.readFileSync('verification_key.json'));
 
 	const res = await snarkjs.groth16.verify(vKey, publicSignals, proof);
 
-	if (res === true) {
-		console.log('Verification OK');
-	} else {
-		console.log('Invalid proof');
-	}
+	assert(res === true, 'Verification should be valid');
 }
 
 run().then(() => {
